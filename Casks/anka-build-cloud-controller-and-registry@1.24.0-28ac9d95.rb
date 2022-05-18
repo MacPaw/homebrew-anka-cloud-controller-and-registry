ea621eb70d746d2a77e183ebfb8eff955d4ced92
Koshevy Anton (astlock) <988910+astlock@users.noreply.github.com>
2022-05-18 08:58:32 +0200

    feat: new version of controller

---
+++ Casks/anka-build-cloud-controller-and-registry@1.24.0-28ac9d95.rb
@@ -0,0 +1,32 @@
+cask "anka-build-cloud-controller-and-registry@1.24.0-28ac9d95" do
+  version "1.24.0-28ac9d95"
+  sha256 "3f4cdbc2d6bfc30d0bb9a30b5f4fe02911391f085e38559c8b0754b60f46f754"
+
+  url "https://downloads.veertu.com/anka/AnkaControllerRegistry-#{version}.pkg"
+  name "Anka Build Cloud Controller & Registry"
+  desc "Virtual machine management GUI/API and registry"
+  homepage "https://veertu.com/"
+
+  livecheck do
+    url "https://veertu.com/downloads/ankacontroller-registry-mac-latest"
+    strategy :header_match
+    regex(/AnkaControllerRegistry[._-]?v?(\d+(?:\.\d+)*[._-]\h+)\.pkg/i)
+  end
+
+  depends_on macos: ">= :yosemite"
+
+  pkg "AnkaControllerRegistry-#{version}.pkg"
+
+  uninstall script: {
+    executable: "/Library/Application Support/Veertu/Anka/tools/controller/uninstall.sh",
+    args:       ["-f"],
+    sudo:       true,
+  }
+
+  zap trash: "/Library/Logs/Veertu/AnkaController",
+      rmdir: "/Library/Application Support/Veertu/Anka/registry"
+
+  caveats do
+    license "https://veertu.com/terms-and-conditions/"
+  end
+end
